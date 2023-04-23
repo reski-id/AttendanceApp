1f2e236d76e030f895e47a9920721957b2e26a7f
reski-id <pesan.reski@gmail.com>
2023-04-23 14:49:07 +0700

    deploy

--- utils/database.go
+++ utils/database.go
@@ -1,27 +1,22 @@
 package utils
 
 import (
-	"fmt"
-	"os"
-
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-
-	"github.com/joho/godotenv"
 )
 
 func Connect() (*gorm.DB, error) {
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println("Error loading .env file")
-		os.Exit(1)
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	fmt.Println("Error loading .env file")
+	// 	os.Exit(1)
+	// }
 
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
+	dbUser := "root"
+	dbPassword := "reski123"
+	dbHost := "localhost"
+	dbPort := "3306"
+	dbName := "attendancedb"
 
 	dsn := dbUser + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
 
